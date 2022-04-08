a29f13378693cd6876e25a3c5f1486ad067da4cd
Elton <elton@ingenie.com>
2022-04-08 07:27:39 +0100

    chore: setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { HomepageComponent } from './core/components/homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// module.exports = {
+//   //mode: 'jit',
+//   purge: {
+//     enabled: true,
+//     content: ['./src/**/*.{html,ts}']
+//   },
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+module.exports = {
+  // mode: 'jit',
+  prefix: '',
+  content: ['./src/**/*.{html,ts}'],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
