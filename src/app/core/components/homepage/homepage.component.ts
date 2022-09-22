2c00b044ccdfe2ec908cf83ec9120051d053e068
Elton <eltonwhybrow@icloud.com>
2022-09-22 16:37:38 +0100

    chore: temp

--- src/app/core/components/homepage/homepage.component.ts
+++ src/app/core/components/homepage/homepage.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-homepage',
@@ -10,6 +11,25 @@ export class HomepageComponent implements OnInit {
   totalAngularPackages: any;
 
   constructor(private http: HttpClient) { }
+
+
   ngOnInit(): void {
+
+    // const loggedIn: any = new BehaviorSubject(true);
+
+    // const loggedIn$ = loggedIn.asObservable();
+
+    // loggedIn$.subscribe((val: any) => console.log('early sub:' + val));
+
+    // loggedIn.next(false)
+    // loggedIn.next(true)
+    // loggedIn.next(false)
+
+    // setTimeout(() => {
+
+    //   loggedIn$.subscribe((val: any) => console.log('late sub:' + val));
+
+    // }, 3000);
+    ;
   }
 }
