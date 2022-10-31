79ecfdf0795ff26d0612af6e6726c4b988cbcb40
Elton <eltonwhybrow@icloud.com>
2022-10-31 18:16:25 +0000

    fix: better route detection for landing pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
-import { AfterViewInit, Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { SeoComponent } from './features/products/pages/seo/seo.component';
 
 @Component({
   selector: 'app-root',
@@ -9,20 +8,31 @@ import { filter } from 'rxjs/operators';
 })
 export class AppComponent {
   landingPage: boolean = false;
+  isLanding: boolean = false;
 
-  constructor(private router: Router) {
+  constructor(
+    // private router: Router
+  ) {
     /*
        Getting current route
        see if its a landing page or not
      */
-    this.router.events.pipe(
-      filter((event: any) => event instanceof NavigationEnd)
-    ).subscribe(event => {
-      if (event.url.includes('/products')) {
-        this.landingPage = true;
-      }
-    })
+    // this.router.events.pipe(
+    //   filter((event: any) => event instanceof NavigationEnd)
+    // ).subscribe(event => {
+    //   if (event.url.includes('/products')) {
+    //     this.landingPage = true;
+    //   }
+    // })
   }
+
+  showHide(event: Component) {
+    console.log('>>>LOG>>>', event);
+    if (event instanceof SeoComponent) { this.isLanding = true } else { this.isLanding = false };
+
+  }
+
+
 
 
 
