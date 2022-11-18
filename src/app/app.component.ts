01d40d6aea813fce00bfdf69f64ae11ebd5dc7c6
Elton <eltonwhybrow@icloud.com>
2022-11-18 08:53:17 +0000

    chore: log comment out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class AppComponent {
   }
 
   showHide(event: Component) {
-    console.log('>>>LOG>>>', event);
+    // console.log('>>>LOG>>>', event);
     if (event instanceof SeoComponent) { this.isLanding = true } else { this.isLanding = false };
 
   }
