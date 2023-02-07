83144e5c509e1c836796877197d9f10226104eda
Elton <eltonwhybrow@icloud.com>
2023-02-07 18:10:25 +0000

    chore: pass tests

--- src/app/features/playground/pages/portfolio/portfolio-details/portfolio-details.component.spec.ts
+++ src/app/features/playground/pages/portfolio/portfolio-details/portfolio-details.component.spec.ts
@@ -3,6 +3,7 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
+
 import { PortfolioDetailsComponent } from './portfolio-details.component';
 
 describe('PortfolioDetailsComponent', () => {
